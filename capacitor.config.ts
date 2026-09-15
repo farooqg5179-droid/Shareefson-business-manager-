a7153cb26902ffa92ffcf0ec0e3bcf0e482f8c69
farooqg5179-droid <farooqg5179@gmail.com>
2026-09-15 14:53:35 +0500

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shareefsons.businessmanager',
+  appName: 'Shareef Sons Business Manager',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
